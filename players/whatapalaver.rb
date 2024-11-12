676f1ca533167b9cdc0c2d7e2a6ed10c5ae31ddf
Angela Wolff <ang@whatapalaver.co.uk>
2024-11-12 16:37:35 +0000

    Weighted random with bitterness

---
+++ players/whatapalaver.rb
@@ -0,0 +1,10 @@
+Player.register("whatapalaver") do |history|
+  options = [:share, :steal]
+  probabilities = [0.7, 0.3]
+  loaded_options = options.zip(probabilities).flat_map { |option, probability| [option] * (probability * 100).to_i }
+  if history.empty?
+    loaded_options.sample
+  else
+    history.last.reject { |name, _move| name == "whatapalaver" }.values.last == :steal ? :steal : loaded_options.sample
+  end
+end

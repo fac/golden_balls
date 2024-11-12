f0d32ef56ffa8fb44cf171b24c4792d01366d620
Dan Corne <dan.corne@freeagent.com>
2024-11-12 16:24:40 +0000

    Simplify logic

--- players/dancorne.rb
+++ players/dancorne.rb
@@ -1,9 +1,5 @@
 # frozen_string_literal: true
 
 Player.register("dancorne") do |history|
-  if history.empty?
-    :share
-  else
-    history.last.reject { |k, v| k == "dancorne" }.values.last == :steal ? :steal : :share
-  end
+  history.empty? ? :share : history.last.reject { |k, v| k == "dancorne" }.values.last
 end
